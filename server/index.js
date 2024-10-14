0ba0800a8aacf6d0e15f56953afd040f40d38950
Ajay1812 <a.kumar01c@gmail.com>
2024-10-14 13:06:49 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const cors = require('cors')
+// const cors = require('cors')
 const adminRouter = require('./routes/admin.js')
 const userRouter = require('./routes/user.js')
 const path = require('path');
 const app = express()
 
 require("dotenv").config();
-app.use(cors())
+// app.use(cors())
 app.use(express.json())
 // app.use('./assets', express.static(path.join(__dirname, 'assets')));
 app.use('/assets', express.static(path.join(__dirname, '../admin-client/assets')));
